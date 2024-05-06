736c90deb9dbb1bdeb87cc5b31044922b8a4bb7c
ismadevjs <agencedz213@gmail.com>
2024-05-06 21:00:38 +0100

    db worked

--- start.sh
+++ start.sh
@@ -4,20 +4,38 @@
 DB_NAME=$1
 
 # Function to run Laravel migrations
-run_migrations() {
-    echo "Running Laravel migrations..."
-    docker compose exec app php artisan migrate 
-    docker compose exec app php artisan db:seed --class=ProviderSeeder
-}
+# run_migrations() {
+#     echo "Running Laravel migrations..."
+#     docker compose exec app php artisan migrate 
+#     docker compose exec app php artisan db:seed --class=ProviderSeeder
+# }
 
 # Run Laravel migrations
-docker compose build && docker compose up -d && docker compose exec mysql mysql -uroot -ptoor -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+docker compose build
+sleep 1
+
+docker compose up -d
+sleep 1
+
+docker compose exec db mysql -uroot -ptoor -e "SHOW DATABASES;"
+sleep 2
+
+docker compose exec db mysql -uroot -ptoor -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+sleep 3
+
+docker compose exec app php artisan migrate
+sleep 5
+
+docker compose exec app php artisan db:seed --class=ProviderSeeder
+sleep 3
+
 
 # Check if MySQL connection error occurred
 if [ $? -ne 0 ]; then
     echo "Error: Can't connect to local MySQL server."
     echo "Retrying migration..."
-    run_migrations
+    # run_migrations
 else
-    run_migrations
+    echo "done."
+    # run_migrations
 fi
