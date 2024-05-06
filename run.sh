bc7a85cba343d1536c00284ae759fd60effceb3a
ismadevjs <agencedz213@gmail.com>
2024-05-06 19:41:01 +0100

    fixed

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@
 cp src/.env.example src/.env
 
 # Edit .env file
-sed -i '' 's/^DB_CONNECTION=sqlite/DB_CONNECTION=mysql/' src/.env
-sed -i '' 's/^# DB_HOST=127\.0\.0\.1/DB_HOST=127.0.0.1/' src/.env
-sed -i '' 's/^# DB_PORT=3306/DB_PORT=3306/' src/.env
-sed -i '' 's/^# DB_DATABASE=laravel/DB_DATABASE=/' src/.env
-sed -i '' 's/^# DB_USERNAME=root/DB_USERNAME=/' src/.env
-sed -i '' 's/^# DB_PASSWORD=/DB_PASSWORD=/' src/.env
+sed -i -e 's/^DB_CONNECTION=sqlite/DB_CONNECTION=mysql/' src/.env
+sed -i -e 's/^# DB_HOST=127\.0\.0\.1/DB_HOST=127.0.0.1/' src/.env
+sed -i -e 's/^# DB_PORT=3306/DB_PORT=3306/' src/.env
+sed -i -e 's/^# DB_DATABASE=laravel/DB_DATABASE=/' src/.env
+sed -i -e 's/^# DB_USERNAME=root/DB_USERNAME=/' src/.env
+sed -i -e 's/^# DB_PASSWORD=/DB_PASSWORD=/' src/.env
 
 # Prompt for database details
 read -p "Enter the database host (default: 127.0.0.1): " DB_HOST
@@ -19,16 +19,16 @@ read -p "Enter the database username: " DB_USERNAME
 read -p "Enter the database password: " DB_PASSWORD
 
 # Update .env file with provided details
-sed -i '' "s/^DB_HOST=.*/DB_HOST=$DB_HOST/" src/.env
-sed -i '' "s/^DB_PORT=.*/DB_PORT=$DB_PORT/" src/.env
-sed -i '' "s/^DB_DATABASE=.*/DB_DATABASE=$DB_DATABASE/" src/.env
-sed -i '' "s/^DB_USERNAME=.*/DB_USERNAME=$DB_USERNAME/" src/.env
-sed -i '' "s/^DB_PASSWORD=.*/DB_PASSWORD=$DB_PASSWORD/" src/.env
-
+sed -i -e "s/^DB_HOST=.*/DB_HOST=$DB_HOST/" src/.env
+sed -i -e "s/^DB_PORT=.*/DB_PORT=$DB_PORT/" src/.env
+sed -i -e "s/^DB_DATABASE=.*/DB_DATABASE=$DB_DATABASE/" src/.env
+sed -i -e "s/^DB_USERNAME=.*/DB_USERNAME=$DB_USERNAME/" src/.env
+sed -i -e "s/^DB_PASSWORD=.*/DB_PASSWORD=$DB_PASSWORD/" src/.env
+sed -i -e "s/MYSQL_DATABASE:.*/MYSQL_DATABASE: $DB_DATABASE/" docker-compose.yml
 echo "Database details updated successfully."
 
 # Update database name in docker-compose.yml
-sed -i '' "s/MYSQL_DATABASE: optizi/MYSQL_DATABASE: $DB_DATABASE/" docker-compose.yml
+sed -i -e "s/MYSQL_DATABASE: optizi/MYSQL_DATABASE: $DB_DATABASE/" docker-compose.yml
 
 # Run 'composer install' inside src/
 echo "Running 'composer install'..."
@@ -49,7 +49,7 @@ fi
 # Check if previous steps were successful, then run start.sh
 if [ $? -eq 0 ]; then
   echo "Setup completed successfully. Starting application..."
-  ./start.sh
+  sudo ./start.sh
 else
   echo "Setup failed. Please check the error messages above."
 fi
